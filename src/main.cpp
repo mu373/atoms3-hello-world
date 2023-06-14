c73cecc7573aa5b3e869b73c0fd555e1b273d7ae
Minami Ueda <minami.ueda@gmail.com>
2023-06-14 10:26:13 +0900

    Format

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,30 @@
 #include <M5Unified.h>
 // https://winchans.com/archives/426
 
-void setup() {
-    auto cfg = M5.config();
-    M5.begin(cfg);
+void setup()
+{
+  auto cfg = M5.config();
+  M5.begin(cfg);
+  Serial.begin(9600);
 
-    M5.Display.init();
-    //画面を反転したくない場合は以下setRotation行をコメントアウト
-    // M5.Display.setRotation(2);
-    M5.Display.startWrite();
-    M5.Display.setFont(&fonts::lgfxJapanGothicP_20);
-    M5.Display.setTextColor(WHITE, BLACK);
-    M5.Display.println("Hello World!");
-    M5.Display.setTextColor(BLUE, BLACK);
-    M5.Display.println("ひらがな");
-    M5.Display.setTextColor(GREEN, BLACK);
-    M5.Display.println("カタカナ");
-    M5.Display.setTextColor(YELLOW, BLACK);
-    M5.Display.println("漢字");
-    M5.Display.endWrite();
+  M5.Display.init();
+  // 画面を反転したくない場合は以下setRotation行をコメントアウト
+  //  M5.Display.setRotation(2);
+  M5.Display.startWrite();
+  M5.Display.setFont(&fonts::lgfxJapanGothicP_20);
+  M5.Display.setTextColor(WHITE, BLACK);
+  M5.Display.println("Hello World!");
+  M5.Display.setTextColor(BLUE, BLACK);
+  M5.Display.println("ひらがな");
+  M5.Display.setTextColor(GREEN, BLACK);
+  M5.Display.println("カタカナ");
+  M5.Display.setTextColor(YELLOW, BLACK);
+  M5.Display.println("漢字");
+  M5.Display.endWrite();
 }
 
-void loop() {
-  
+void loop()
+{
+  Serial.println("Hello world!");
+  delay(1000);
 }
